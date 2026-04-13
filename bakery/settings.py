7b4e73e9da1c54b3ac3f940e1ceb7031b29bce52
Nhannguyencoder <chogaowetui@gmail.com>
2026-04-13 20:30:04 +0700

    setup deploy

--- bakery/settings.py
+++ bakery/settings.py
@@ -24,8 +24,8 @@ SECRET_KEY = 'django-insecure-+e5ue2b5#u_ga50!=vww)s%eqv_&!3oq8a5zmp$m6p(h)p!ef$
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
+import os
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
